4a095a65f25cc1939ec811fd51fc17da0a3a7b5b
codes-harshit <programming.harshit44@gmail.com>
2025-03-26 17:01:12 +0530

    frontend setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,20 @@
 import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+
+import HomePage from "./pages/HomePage.jsx";
+import SignupPage from "./pages/SignupPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <>Client Side</>;
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
